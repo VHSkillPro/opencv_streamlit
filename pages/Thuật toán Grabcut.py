5792e86d22a25ce1b25aa5f8150dc647a9928b4a
Hazzu <hailaconsutu558@gmail.com>
2024-09-30 19:38:19 +0700

    [FIX] Fix grabcut site

--- pages/Thuật toán Grabcut.py
+++ pages/Thuật toán Grabcut.py
@@ -1,10 +1,9 @@
-import cv2, os
+import cv2
 import numpy as np
 from PIL import Image
 import streamlit as st
-from streamlit_drawable_canvas import st_canvas
-
 from services.grabcut.grabcut import grabcut
+from streamlit_drawable_canvas import st_canvas
 
 st.set_page_config(
     page_title="Ứng dụng thuật toán GrabCut",
@@ -13,41 +12,43 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 st.title("Ứng dụng thuật toán GrabCut")
-
-st.subheader("1. Chọn ảnh cần xử lý")
 uploaded_image = st.file_uploader(
     "Chọn hoặc kéo ảnh vào ô bên dưới", type=["jpg", "jpeg", "png"]
 )
 
 if uploaded_image is not None:
-    st.subheader("2. Chọn vùng cần xử lý bằng cách kéo chuột trên ảnh")
-    cols = st.columns(2)
+    st.write(":pencil: **Chọn vùng cần tách nền bằng cách kéo thả chuột trong ảnh**")
+    st.write(":pencil: **Sau đó nhấn vào nút `Tách nền`**")
+    submitBtn = st.button("Tách nền")
+
+    cols = st.columns(2, gap="large")
+    raw_image = Image.open(uploaded_image)
 
     with cols[0]:
-        image = Image.open(uploaded_image)
-        w, h = image.size
+        w, h = raw_image.size
+        width = min(w, 475)
+        height = width * h // w
 
-        st.columns(3)[1].write("Ảnh gốc")
         canvas_result = st_canvas(
-            background_image=image,
+            background_image=raw_image,
             drawing_mode="rect",
-            fill_color="rgba(255, 165, 0, 0.2)",
+            fill_color="rgba(255, 165, 0, 0.1)",
             stroke_width=1,
-            width=400,
-            height=400 * h // w,
+            width=width,
+            height=height,
         )
 
-    with cols[1]:
-        if canvas_result.json_data is not None:
-            if len(canvas_result.json_data.get("objects")) > 1:
-                st.error("Chỉ được chọn một vùng cần xử lý")
-            elif len(canvas_result.json_data.get("objects")) == 1:
-                st.columns(3)[1].write("Ảnh đã qua xử lý")
+    if canvas_result.json_data is not None and submitBtn:
+        if len(canvas_result.json_data.get("objects")) > 1:
+            st.error("Chỉ được chọn một vùng cần tách nền")
+        elif len(canvas_result.json_data.get("objects")) < 1:
+            st.error("Vui lòng chọn một vùng cần tách nền")
+        elif len(canvas_result.json_data.get("objects")) == 1:
+            with st.spinner("Đang xử lý..."):
+                orginal_image = np.array(raw_image)
+                orginal_image = cv2.cvtColor(orginal_image, cv2.COLOR_RGBA2BGR)
+                scale = orginal_image.shape[1] / width
 
-                orginal_image = np.array(image)
-                orginal_image = cv2.cvtColor(orginal_image, cv2.COLOR_RGB2BGR)
-
-                scale = w / 400
                 min_x = canvas_result.json_data.get("objects")[0]["left"]
                 min_y = canvas_result.json_data.get("objects")[0]["top"]
                 width = canvas_result.json_data.get("objects")[0]["width"]
@@ -62,4 +63,5 @@ if uploaded_image is not None:
                         int(height * scale),
                     ),
                 )
-                st.image(res, channels="BGR", width=400)
+
+                cols[1].image(res, channels="BGR", caption="Ảnh kết quả")
