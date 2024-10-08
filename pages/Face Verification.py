62c8b6c636d2f156638693584b5e97680999440f
Hazzu <hailaconsutu558@gmail.com>
2024-10-08 20:56:24 +0700

    [ADD] Add manage students

--- pages/Face Verification.py
+++ pages/Face Verification.py
@@ -1,15 +1,145 @@
-import json
-import datetime
 import pandas as pd
 from PIL import Image
 import streamlit as st
-from google.cloud import firestore, storage
+from services.face_verification.db import StudentService
 
-key_dict = json.loads(st.secrets["textkey"])
-db = firestore.Client.from_service_account_info(key_dict)
-bucket = storage.Client.from_service_account_info(key_dict).get_bucket(
-    "face-recognize-7d75c.appspot.com"
-)
+studentService = StudentService()
+
+if "submit_form" not in st.session_state:
+    st.session_state["submit_form"] = False
+
+if "show_form_add" not in st.session_state:
+    st.session_state["show_form_add"] = False
+
+
+@st.cache_data(ttl="1h")
+def get_table_data():
+    students = studentService.get_all()
+    table_data = {"checkbox": [], "id": [], "card": [], "selfie": []}
+    for id, student in students.items():
+        table_data["checkbox"].append(False)
+        table_data["id"].append(student["id"])
+        table_data["card"].append(studentService.get_url_from_storage(student["card"]))
+        table_data["selfie"].append(
+            studentService.get_url_from_storage(student["selfie"], 3600)
+        )
+    return table_data
+
+
+def display_table_students():
+    table_data = get_table_data()
+    if len(table_data["id"]) == 0:
+        st.write("Không có sinh viên nào.")
+        return pd.DataFrame(table_data)
+
+    return st.data_editor(
+        pd.DataFrame(table_data),
+        column_config={
+            "checkbox": st.column_config.CheckboxColumn("Chọn"),
+            "id": st.column_config.TextColumn("Mã sinh viên", disabled=True),
+            "card": st.column_config.ImageColumn("Thẻ sinh viên"),
+            "selfie": st.column_config.ImageColumn("Chân dung"),
+        },
+        use_container_width=True,
+        hide_index=True,
+    )
+
+
+def display_manage_students():
+    def show_form_add():
+        st.session_state["show_form_add"] = True
+        st.session_state["submit_form"] = False
+
+    def hide_form_add():
+        st.session_state["show_form_add"] = False
+        st.session_state["submit_form"] = False
+
+    st.header("1. Danh sách sinh viên")
+    container_action = st.container()
+    container_form_add = st.container()
+    data_editor = display_table_students()
+    st.caption("- Click đúp vào ô ảnh cần xem để phóng to ảnh.")
+
+    with container_action:
+        cols = st.columns([1, 1, 8])
+        with cols[0]:
+            st.button("Thêm", use_container_width=True, on_click=show_form_add)
+        with cols[1]:
+
+            @st.dialog("Bạn có chắc chắn muốn xóa sinh viên đã chọn không?")
+            def handle_remove_students():
+                st.write("Danh sách sinh viên sẽ không thể khôi phục sau khi xóa.")
+                st.write("Hãy chắc chắn trước khi xóa.")
+                if st.button("Xác nhận"):
+                    ids = data_editor[data_editor["checkbox"] == True]["id"].tolist()
+                    haveChanges = False
+                    for id in ids:
+                        if studentService.delete(id):
+                            haveChanges = True
+                            st.toast(
+                                f"Xóa sinh viên {id} thành công",
+                                icon=":material/check:",
+                            )
+                        else:
+                            st.toast(
+                                f"Xóa sinh viên {id} thất bại", icon=":material/close:"
+                            )
+
+                    if haveChanges:
+                        st.cache_data.clear()
+                        st.rerun()
+
+            if (data_editor["checkbox"] == True).sum() > 0:
+                st.button(
+                    "Xóa", use_container_width=True, on_click=handle_remove_students
+                )
+
+    if st.session_state["show_form_add"]:
+        with container_form_add:
+            with st.form("form_add"):
+                st.markdown("#### Thêm sinh viên mới")
+                id = st.text_input("Mã sinh viên")
+                container_error_id = st.container()
+                with container_error_id:
+                    if st.session_state["submit_form"] and id.strip() == "":
+                        st.caption(":red[Mã sinh viên không được để trống]")
+
+                cols = st.columns(2)
+                with cols[0]:
+                    card = st.file_uploader("Thẻ sinh viên")
+                    if st.session_state["submit_form"] and card is None:
+                        st.caption(":red[Ảnh thẻ sinh viên không được để trống]")
+
+                with cols[1]:
+                    selfie = st.file_uploader("Chân dung")
+                    if st.session_state["submit_form"] and selfie is None:
+                        st.caption(":red[Ảnh chân dung không được để trống]")
+
+                def handle_add():
+                    st.session_state["submit_form"] = True
+                    if id.strip() == "" or card is None or selfie is None:
+                        return
+
+                    isCreated = studentService.add(id, card, selfie)
+                    if not isCreated:
+                        container_error_id.caption(":red[Mã sinh viên đã tồn tại]")
+
+                    st.cache_data.clear()
+                    hide_form_add()
+                    st.toast("Thêm sinh viên thành công", icon=":material/check:")
+                    st.rerun()
+
+                cols = st.columns([1, 1, 8])
+                btn_submit = cols[0].form_submit_button(
+                    "Thêm", use_container_width=True
+                )
+                cols[1].form_submit_button(
+                    "Hủy", use_container_width=True, on_click=hide_form_add
+                )
+
+                if btn_submit:
+                    handle_add()
+
 
 st.set_page_config(
     page_title="Face Verification",
@@ -18,36 +148,5 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-st.title("Dữ liệu trong Firestore")
-
-docs = db.collection("TheSV").stream()
-data_table = {"id": [], "studentId": [], "path": [], "image": []}
-for doc in docs:
-    data_table["id"].append(doc.id)
-    data_table["studentId"].append(doc.to_dict()["studentId"])
-    data_table["path"].append(doc.to_dict()["path"])
-
-    path = doc.to_dict()["path"].replace("gs://face-recognize-7d75c.appspot.com/", "")
-    public_url = bucket.blob(path).generate_signed_url(
-        datetime.timedelta(seconds=300), method="GET"
-    )
-    data_table["image"].append(f"<img src='{public_url}' width='150px'>")
-
-st.header("1. Collection TheSV")
-st.write(pd.DataFrame(data_table).to_html(escape=False), unsafe_allow_html=True)
-
-docs_2 = db.collection("ChanDung").stream()
-data_table_2 = {"id": [], "studentId": [], "path": [], "image": []}
-for doc in docs_2:
-    data_table_2["id"].append(doc.id)
-    data_table_2["studentId"].append(doc.to_dict()["studentId"])
-    data_table_2["path"].append(doc.to_dict()["path"])
-
-    path = doc.to_dict()["path"].replace("gs://face-recognize-7d75c.appspot.com/", "")
-    public_url = bucket.blob(path).generate_signed_url(
-        datetime.timedelta(seconds=300), method="GET"
-    )
-    data_table_2["image"].append(f"<img src='{public_url}' width='150px'>")
-
-st.header("2. Collection ChanDung")
-st.write(pd.DataFrame(data_table_2).to_html(escape=False), unsafe_allow_html=True)
+st.title("Ứng dụng xác thực khuôn mặt")
+display_manage_students()
---
+++ services/face_verification/db.py
@@ -0,0 +1,102 @@
+import json
+import datetime
+import numpy as np
+import streamlit as st
+from google.cloud import firestore, storage
+from google.cloud.firestore import FieldFilter
+from streamlit.runtime.uploaded_file_manager import UploadedFile
+
+
+class Service:
+    def __init__(self) -> None:
+        key_dict = json.loads(st.secrets["textkey"])
+        self.db = firestore.Client.from_service_account_info(key_dict)
+        self.bucket = storage.Client.from_service_account_info(key_dict).get_bucket(
+            "face-recognize-7d75c.appspot.com"
+        )
+
+    def parse_data(self, docs):
+        result = {}
+        for doc in docs:
+            result[doc.id] = doc.to_dict()
+        return result
+
+    def get_all(self, collection):
+        print(
+            datetime.datetime.now(), ">> Get all documents from collection", collection
+        )
+        docs = self.db.collection(collection).stream()
+        return self.parse_data(docs)
+
+    def add(self, collection, data):
+        print(datetime.datetime.now(), ">> Add new document to collection", collection)
+        new_doc_ref = self.db.collection(collection).document()
+        new_doc_ref.set(data)
+        return True
+
+    def delete(self, collection, id):
+        print(datetime.datetime.now(), ">> Delete document from collection", collection)
+        self.db.collection(collection).document(id).delete()
+        return True
+
+    def get_url_from_storage(self, path, expires_in=300):
+        print(datetime.datetime.now(), ">> Get public url from storage", path)
+        public_url = self.bucket.blob(path).generate_signed_url(
+            datetime.timedelta(seconds=expires_in), method="GET"
+        )
+        return public_url
+
+
+class StudentService(Service):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def get_all(self):
+        result = super().get_all("students")
+        return result
+
+    def find_by(self, id: str):
+        print(datetime.datetime.now(), ">> Find student by id", id)
+        docs = (
+            self.db.collection("students")
+            .where(filter=FieldFilter("id", "==", id))
+            .stream()
+        )
+        return self.parse_data(docs)
+
+    def add(self, id: str, card: UploadedFile, selfie: UploadedFile) -> bool:
+        if id.strip() == "" or card is None or selfie is None:
+            return
+
+        if len(self.find_by(id)) > 0:
+            return False
+
+        print(datetime.datetime.now(), ">> Add new student")
+
+        # Upload images to storage
+        card_path = f"TheSV/{card.name}"
+        self.bucket.blob(card_path).upload_from_file(card)
+
+        selfie_path = f"ChanDung/{selfie.name}"
+        self.bucket.blob(selfie_path).upload_from_file(selfie)
+
+        data = {
+            "id": id,
+            "card": card_path,
+            "selfie": selfie_path,
+        }
+
+        return super().add("students", data)
+
+    def delete(self, id: str) -> bool:
+        if id.strip() == "":
+            return
+
+        students = self.find_by(id)
+        if len(students) == 0:
+            return False
+
+        print(datetime.datetime.now(), ">> Delete student")
+
+        student = list(students.values())[0]
+        return super().delete("students", list(students.keys())[0])
