ac973f5c92db516c031be528a55a3788799306ff
Hazzu <hailaconsutu558@gmail.com>
2024-10-10 02:26:27 +0700

    [ADD] Add face verification in selfie and card image

--- pages/Face Verification.py
+++ pages/Face Verification.py
@@ -1,9 +1,35 @@
+import cv2
+import numpy as np
 import pandas as pd
-from PIL import Image
+from PIL import Image, ImageOps
 import streamlit as st
+from services.face_verification.service import visualize
+from services.face_verification.sface import SFace
 from services.face_verification.db import StudentService
+from services.face_verification.yunet import YuNet
+
+__SERVICE_PATH__ = "services/face_verification"
 
 studentService = StudentService()
+
+backend_target_pairs = [
+    [cv2.dnn.DNN_BACKEND_OPENCV, cv2.dnn.DNN_TARGET_CPU],
+    [cv2.dnn.DNN_BACKEND_CUDA, cv2.dnn.DNN_TARGET_CUDA],
+    [cv2.dnn.DNN_BACKEND_CUDA, cv2.dnn.DNN_TARGET_CUDA_FP16],
+    [cv2.dnn.DNN_BACKEND_TIMVX, cv2.dnn.DNN_TARGET_NPU],
+    [cv2.dnn.DNN_BACKEND_CANN, cv2.dnn.DNN_TARGET_NPU],
+]
+
+backend_id, target_id = backend_target_pairs[0]
+
+recognizer = SFace(
+    modelPath=f"{__SERVICE_PATH__}/misc/face_recognition_sface_2021dec.onnx",
+    disType=0,
+    backendId=backend_id,
+    targetId=target_id,
+)
+
+# ----------------------------------------------
 
 if "submit_form" not in st.session_state:
     st.session_state["submit_form"] = False
@@ -141,6 +167,88 @@ def display_manage_students():
                     handle_add()
 
 
+def display_face_verification():
+    st.header("2. Ứng dụng xác thực ảnh chân dung và thẻ sinh viên")
+    with st.form("form_face_verification"):
+        confThreshold = st.slider(
+            "Chọn **confidence threshold** để phát hiện khuôn mặt", 0.0, 1.0, 0.9, 0.01
+        )
+        cols = st.columns(2)
+        card = cols[0].file_uploader("Thẻ sinh viên")
+        selfie = cols[1].file_uploader("Chân dung")
+        btn_submit = st.form_submit_button("Xác thực")
+        st.caption(
+            """
+                - Ảnh thẻ sinh viên và chân dung phải rõ ràng, không bị mờ.
+                - Đảm bảo kích thước khuôn mặt trên ảnh thẻ sinh viên và chân dung nằm trong khoảng $10$x$10$ đến $300$x$300$ pixels.
+            """
+        )
+
+    if btn_submit:
+        if (card is None) or (selfie is None):
+            st.warning("Vui lòng chọn ảnh thẻ sinh viên và chân dung.")
+        else:
+            detector = YuNet(
+                modelPath=f"{__SERVICE_PATH__}/misc/face_detection_yunet_2023mar.onnx",
+                confThreshold=confThreshold,
+                nmsThreshold=0.3,
+                topK=5000,
+                backendId=backend_id,
+                targetId=target_id,
+            )
+
+            card = Image.open(card)
+            card = ImageOps.exif_transpose(card)
+            card = cv2.cvtColor(np.array(card), cv2.COLOR_RGB2BGR)
+            detector.setInputSize([card.shape[1], card.shape[0]])
+            faces_card = detector.infer(card)
+            if len(faces_card) == 0:
+                st.warning(
+                    "Không tìm thấy khuôn mặt trên thẻ sinh viên. Vui lòng chọn ảnh khác hoặc điều chỉnh **confidence threshold**."
+                )
+
+            selfie = Image.open(selfie)
+            selfie = ImageOps.exif_transpose(selfie)
+            selfie = cv2.cvtColor(np.array(selfie), cv2.COLOR_RGB2BGR)
+            detector.setInputSize([selfie.shape[1], selfie.shape[0]])
+            faces_selfie = detector.infer(selfie)
+            if len(faces_selfie) == 0:
+                st.warning(
+                    "Không tìm thấy khuôn mặt trên ảnh chân dung. Vui lòng chọn ảnh khác hoặc điều chỉnh **confidence threshold**."
+                )
+
+            if len(faces_card) > 0 and len(faces_selfie) > 0:
+                scores = []
+                matches = []
+
+                for face in faces_card:
+                    result = recognizer.match(
+                        selfie, faces_selfie[0][:-1], card, face[:-1]
+                    )
+                    scores.append(result[0])
+                    matches.append(result[1])
+
+                selfie, card = visualize(
+                    selfie, faces_selfie, card, faces_card, matches, scores
+                )
+
+                cols = st.columns(2)
+                cols[0].image(
+                    selfie,
+                    caption="Chân dung",
+                    use_column_width=True,
+                    channels="BGR",
+                )
+                cols[1].image(
+                    card,
+                    caption="Thẻ sinh viên",
+                    use_column_width=True,
+                    channels="BGR",
+                )
+
+
+# ----------------------------------------------
+
 st.set_page_config(
     page_title="Face Verification",
     page_icon=Image.open("./public/images/logo.png"),
@@ -150,3 +258,4 @@ st.set_page_config(
 
 st.title("Ứng dụng xác thực khuôn mặt")
 display_manage_students()
+display_face_verification()
