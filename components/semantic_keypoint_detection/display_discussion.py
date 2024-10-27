0d63d384addb5d0ab369eb3be0456ce74a704f30
Hazzu <hailaconsutu558@gmail.com>
2024-10-28 00:03:23 +0700

    [UPDATE] - Update discussion semantic keypoint detection

--- components/semantic_keypoint_detection/display_discussion.py
+++ components/semantic_keypoint_detection/display_discussion.py
@@ -4,3 +4,10 @@ import streamlit as st
 @st.fragment()
 def display_discussion():
     st.header("5. Thảo luận")
+    st.write(
+        """
+        - **ORB** có ưu thế hơn **SIFT** cả **precision** và **recall** khi phát hiện keypoints trên $4$ loại hình checkboard, cube, polygon và star.
+        - **SIFT** có ưu thế hơn **ORB** cả **precision** và **recall** khi phát hiện keypoints trên $2$ loại hình lines và stripes.
+        - Ở loại hình **multiple polygons**, **ORB** có **precision** cao hơn **SIFT** nhưng **recall** lại thấp hơn.
+        """
+    )
