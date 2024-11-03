316bd95809675a415e6905ca5a71039a280492d8
Hazzu <hailaconsutu558@gmail.com>
2024-11-04 00:39:13 +0700

    .

--- services/image_search_engine/services.py
+++ services/image_search_engine/services.py
@@ -18,6 +18,12 @@ def search_image(img: cv2.typing.MatLike, top_k: int, mybar=None) -> list[str]:
     """
     gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     _, query_descriptors = orb.detectAndCompute(gray_scale, None)
+    gray_scale_90 = cv2.rotate(gray_scale, cv2.ROTATE_90_CLOCKWISE)
+    _, query_descriptors_90 = orb.detectAndCompute(gray_scale_90, None)
+    gray_scale_180 = cv2.rotate(gray_scale, cv2.ROTATE_180)
+    _, query_descriptors_180 = orb.detectAndCompute(gray_scale_180, None)
+    gray_scale_270 = cv2.rotate(gray_scale, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    _, query_descriptors_270 = orb.detectAndCompute(gray_scale_270, None)
 
     results = []
     for image_name in images_name:
@@ -27,8 +33,16 @@ def search_image(img: cv2.typing.MatLike, top_k: int, mybar=None) -> list[str]:
         )
 
         matches = bf.match(query_descriptors, descriptors)
-        matches = sorted(matches, key=lambda x: x.distance)
-        results.append((image_name, len(matches)))
+        matches_90 = bf.match(query_descriptors_90, descriptors)
+        matches_180 = bf.match(query_descriptors_180, descriptors)
+        matches_270 = bf.match(query_descriptors_270, descriptors)
+
+        results.append(
+            (
+                image_name,
+                max(len(matches), len(matches_90), len(matches_180), len(matches_270)),
+            )
+        )
 
         if mybar is not None:
             mybar.progress(len(results) / len(images_name))
