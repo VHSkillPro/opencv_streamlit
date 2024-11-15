c909474bbbd1c6a41dbfe3921eeef946d4b9b3a6
Hazzu <hailaconsutu558@gmail.com>
2024-11-15 15:10:53 +0700

    .

--- components/image_search_engine/__init__.py
+++ components/image_search_engine/__init__.py
@@ -15,22 +15,13 @@ def load_cache():
     DATASET_DIR = "./services/image_search_engine/val2017"
     images_name = os.listdir(os.path.join(DATASET_DIR, "images"))
     descriptors = []
-    keypoints = []
 
     for image_name in images_name:
         image_name_no_ext = os.path.splitext(image_name)[0]
         descriptor = np.load(
             os.path.join(DATASET_DIR, "descriptors", f"{image_name_no_ext}.npy")
         )
-        keypoint = np.load(
-            os.path.join(DATASET_DIR, "keypoints", f"{image_name_no_ext}.npy")
-        )
-        keypoint = [
-            cv2.KeyPoint(kp[0], kp[1], kp[2], kp[3], kp[4], int(kp[5]), int(kp[6]))
-            for kp in keypoint
-        ]
 
         descriptors.append(descriptor)
-        keypoints.append(keypoint)
 
-    return images_name, descriptors, keypoints
+    return images_name, descriptors
